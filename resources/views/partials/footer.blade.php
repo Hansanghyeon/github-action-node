05f1debb390227ffdf0b2ec6034c2b4a05d9d656
999hyeon <999@hyeon.pro>
2021-08-01 16:29:46 +0900

    initial

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,17 @@
+<footer id="global" class="content-info global">
+  <div class="container lg:max-w-screen-lg mx-auto grid grid-flow-row auto-rows-max gap-3">
+    <x-VisitorCounter />
+    <a href="/history">
+      Since 2014.7.22 - <span id="history"></span>
+    </a>
+    <span>
+      Copyright (c) 2021 999hyeon
+    </span>
+    @php dynamic_sidebar('sidebar-footer') @endphp
+  </div>
+</footer>
+@if (current_user_can('editor') || current_user_can('administrator')) 
+<div class="share-global">
+  <x-Share />
+</div>
+@endif
